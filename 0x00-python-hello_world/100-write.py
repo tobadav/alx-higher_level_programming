58a2ed7d26d8bbf8ec08d7f58069447562ce1ac5
tobadav <tobadav@gmail.com>
2023-01-04 13:35:31 -0800

    write

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import sys
 
-sys.stderr.write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19")
+sys.stderr.write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n")
