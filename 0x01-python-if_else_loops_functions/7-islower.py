af506475716c7c8132a300d996126c9200e192cf
tobadav <tobadav@gmail.com>
2023-01-07 04:36:21 -0800

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -4,5 +4,6 @@ def islower(c):
     for i in c:
         if ord(c) >= 97 and ord(c) <= 122:
             return True
-        elif ord(c) >= 65 and ord(c) <= 90:
+    for i in c:
+        if ord(c) >= 65 and ord(c) <= 90:
             return False
