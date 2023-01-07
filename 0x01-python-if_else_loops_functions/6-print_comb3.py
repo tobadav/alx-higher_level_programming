95a2958ef57c33ace7d5ffe48825599443014340
tobadav <tobadav@gmail.com>
2023-01-07 12:24:42 -0800

    print combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,7 @@
 for f in range(0, 10):
     for s in range(1, 10):
         if s > f:
-            print(f"{s}{f}", end="")
+            print("{}{}".format(f, s), end='')
             if int(str(f) + str(s)) != 89:
                 print(end=",")
+print()
