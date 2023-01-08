95d8aa005f4473ccd87875bb81f2903c392d9368
tobadav <tobadav@gmail.com>
2023-01-08 09:33:49 -0800

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for fizzbuzz in range(1, 100):
-        if fizzbuzz % 15 == 0:
+        if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
             print("FizzBuzz", end=" ")
             continue
         elif fizzbuzz % 3 == 0:
