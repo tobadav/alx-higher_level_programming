1e849c2c0652133fb5f77809aad131ac870dd9b4
tobadav <tobadav@gmail.com>
2023-02-01 09:04:34 -0800

    print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,45 +1,23 @@
 #!/usr/bin/python3
 """
-This module is composed by a function that prints a square with the character #
+    This is the ``say_my_name`` module.
+    The say_my_name module supplies one function, say_my_name().
 """
 
 
-def print_square(size):
-    """ Function that prints a square with the character #
-    Args:
-        size: size of the square printed
-    Returns:
-        No return
-    Raises:
-        TypeError: If size is not an integer number
+def say_my_name(first_name, last_name=""):
+    """
+        this functions prints a string:
+        e.g: My namem is <fist name> <last name>
+        Args:
+            first_name (str): The first name
+            last_name (str): The last name (Optional)
     """
 
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
 
-    for i in range(size):
-        print("#" * size)#!/usr/bin/python3
-"""
-This module is composed by a function that prints a square with the character #
-"""
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
 
-
-def print_square(size):
-    """ Function that prints a square with the character #
-    Args:
-        size: size of the square printed
-    Returns:
-        No return
-    Raises:
-        TypeError: If size is not an integer number
-    """
-
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-
-    for i in range(size):
-        print("#" * size)
+    print("My name is {:s} {:s}".format(first_name, last_name))
