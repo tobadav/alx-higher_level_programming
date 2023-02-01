0792b3e8a012cdee07a337359ba20deaa7a03cf3
tobadav <tobadav@gmail.com>
2023-02-01 08:40:16 -0800

    matrix divided

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
 """
-    This is the ``say_my_name`` module.
-    The say_my_name module supplies one function, say_my_name().
+This is the "3-say_my-name" module.
+The 3-say_my_name  module supplies one function, say_my_name.
 """
 
-def say_my_name(first_name, last_name=""):
-    """
-        this functions prints a string:
-        e.g: My namem is <fist name> <last name>
-        Args:
-            first_name (str): The first name
-            last_name (str): The last name (Optional)
-    """
 
+def say_my_name(first_name, last_name=""):
+    """Prints "My name is" followed by the first name and optional last name"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-
-    print("My name is {:s} {:s}".format(first_name, last_name))
+    print("My name is", first_name, last_name)
