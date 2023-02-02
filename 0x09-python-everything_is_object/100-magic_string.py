1242113b5b350f9dbf6cf48cc65dbe19ec69566b
tobadav <tobadav@gmail.com>
2023-02-02 11:47:57 -0800

    All Task

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""This defines a locked class"""
-
-
-class LockedClass:
-    """
-    Only allows instatiation of an attribute called first_name
-    """
-
-    __slots__ = ["first_name"]
+def magic_string(H=[]):
+    H += ["BestSchool"]
+    return (", ".join(H))
