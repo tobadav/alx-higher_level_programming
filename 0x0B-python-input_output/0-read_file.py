9c2651ec1418a241102d60c4a71ddadb5db3720d
tobadav <tobadav@gmail.com>
2023-02-09 01:18:06 -0800

    All Task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-"""
-0-read_file module
-"""
+"""This module defines a text file-reading function"""
+
 
 def read_file(filename=""):
-    """
-    read_file - reads a text file (UTF8) and prints it to stdout
-    Args:
-        filename: name of the file
-    """
-    with open(filename, "r", encoding="UTF-8") as f:
-        for line in f:
-            print(line, end="")
+    """Prints the contents of a UTF8 text file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,6 +4,7 @@
 """
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """
     save_to_json_file -  writes an Object to a text file,
